a9f54daaf28195e552e29c8f1d0b4721e31eafb2
Robin Richard-Canavaggio <22174078+r-richardcanavaggio@users.noreply.github.com>
2025-12-18 13:20:18 +0100

    added operators overload for matrix class

--- types/includes/Matrix.hpp
+++ types/includes/Matrix.hpp
@@ -6,7 +6,7 @@
 /*   By: rrichard <rrichard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 16:28:09 by rrichard          #+#    #+#             */
-/*   Updated: 2025/12/17 12:06:19 by rrichard         ###   ########.fr       */
+/*   Updated: 2025/12/18 13:16:50 by rrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,17 +35,20 @@ class Matrix
 		size_t					getRows() const;
 		size_t					getCols() const;
 		bool					empty() const;
-		double					at( size_t	row, size_t col );
-		const double			at( size_t	row, size_t col ) const;
-		double					operator()( size_t row, size_t col );
-		const double			operator()( size_t row, size_t col ) const;
-		double					operator[]( size_t index );
-		const double			operator[]( size_t index ) const;
+		double&					at( size_t row, size_t col );
+		const double&			at( size_t row, size_t col ) const;
+		double&					operator()( size_t row, size_t col );
+		const double&			operator()( size_t row, size_t col ) const;
+		double&					operator[]( size_t index );
+		const double&			operator[]( size_t index ) const;
 
 		friend std::ostream&	operator<<( std::ostream& os, const Matrix& m );
 
-		// ex00
 		void					add( const Matrix& m );
 		void					sub( const Matrix& m );
-		void					scl( const double scalar );
+		void					scl( const double& scalar );
+		Matrix					operator+( const Matrix& ) const;
+		Matrix					operator-( const Matrix& ) const;
+		Matrix					operator*( const Matrix& ) const;
+		Matrix					operator*( const double& scalar ) const;
 };
--- types/srcs/Matrix.cpp
+++ types/srcs/Matrix.cpp
@@ -6,7 +6,7 @@
 /*   By: rrichard <rrichard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 12:10:48 by rrichard          #+#    #+#             */
-/*   Updated: 2025/12/17 12:08:18 by rrichard         ###   ########.fr       */
+/*   Updated: 2025/12/18 13:17:33 by rrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,38 +67,38 @@ bool			Matrix::empty() const
 	return (elements.empty());
 }
 
-double			Matrix::at( size_t row, size_t col )
+double&			Matrix::at( size_t row, size_t col )
 {
 	if (row >= this->_rows || col >= this->_cols)
 		throw std::out_of_range("Matrix::at: index out of range: ");
 	return (elements[row * _cols + col]);
 }
 
-const double	Matrix::at( size_t row, size_t col ) const
+const double&	Matrix::at( size_t row, size_t col ) const
 {
 	if (row >= this->_rows || col >= this->_cols)
 		throw std::out_of_range("Matrix::at: index out of range: ");
 	return (elements[row * _cols + col]);
 }
 
-double			Matrix::operator()( size_t row, size_t col )
+double&			Matrix::operator()( size_t row, size_t col )
 {
 	return (at(row, col));
 }
 
-const double	Matrix::operator()( size_t row, size_t col ) const
+const double&	Matrix::operator()( size_t row, size_t col ) const
 {
 	return (at(row, col));
 }
 
-double			Matrix::operator[]( size_t index )
+double&			Matrix::operator[]( size_t index )
 {
 	if (index >= this->getSize())
 		throw std::runtime_error("Error: index out of bound.");
 	return (this->elements[index]);
 }
 
-const double	Matrix::operator[]( size_t index ) const
+const double&	Matrix::operator[]( size_t index ) const
 {
 	if (index >= this->getSize())
 		throw std::runtime_error("Error: index out of bound.");
@@ -118,4 +118,71 @@ std::ostream&	operator<<( std::ostream& os, const Matrix& m )
 			os << ", ";
 	}
 	return (os);
+}
+
+void	Matrix::add( const Matrix& m )
+{
+	if (m._cols != this->_cols || m._rows != this->_rows)
+		throw std::runtime_error("Error: Matrices must have the same dimensions for addition");
+	for (size_t i = 0; i < elements.size(); i++)
+		this->elements[i] += m.elements[i];
+}
+
+void	Matrix::sub( const Matrix& m )
+{
+	if (m._cols != this->_cols || m._rows != this->_rows)
+		throw std::runtime_error("Error: Matrices must have the same dimensions for substraction");
+	for (size_t i = 0; i < elements.size(); i++)
+		this->elements[i] -= m.elements[i];
+}
+
+void	Matrix::scl( const double& scalar )
+{
+	for (size_t i = 0; i < this->getSize(); i++)
+		this->elements[i] *= scalar;
+}
+
+
+Matrix	Matrix::operator+( const Matrix& other ) const
+{
+	Matrix	m = *this;
+
+	m.add(other);
+	return (m);
+}
+
+Matrix	Matrix::operator-( const Matrix& other ) const
+{
+	Matrix	m = *this;
+
+	m.sub(other);
+	return (m);
+}
+
+Matrix	Matrix::operator*( const Matrix& other ) const
+{
+	if (this->_cols != other._rows)
+		throw std::runtime_error("Error: incompatible dimensions for matrix multiplication");
+
+	Matrix	result(this->_rows, other._cols);
+	
+	for (size_t i = 0; i < this->_rows; i++)
+	{
+		for (size_t j = 0; j < other._cols; j++)
+		{
+			double	sum = 0.0;
+			for (size_t k = 0; k < this->_cols; k++)
+				sum += this->at(i, k) * other.at(k, j);
+			result(i, j) = sum;
+		}
+	}
+	return (result);
+}
+
+Matrix	Matrix::operator*( const double& scalar ) const
+{
+	Matrix	m = *this;
+
+	m.scl(scalar);
+	return (m);
 }
