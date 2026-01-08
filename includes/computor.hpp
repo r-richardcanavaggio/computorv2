1e965a768fe81abfac8e6d131186a92776def998
Robin Richard-Canavaggio <22174078+r-richardcanavaggio@users.noreply.github.com>
2026-01-08 16:30:23 +0100

    deleted MathVisitor definition from hpp, added arity enum for unary/binary operators logic

--- includes/computor.hpp
+++ includes/computor.hpp
@@ -6,7 +6,7 @@
 /*   By: rrichard <rrichard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 09:40:35 by rrichard          #+#    #+#             */
-/*   Updated: 2026/01/08 10:08:59 by rrichard         ###   ########.fr       */
+/*   Updated: 2026/01/08 16:11:29 by rrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,73 +25,24 @@ enum TokenType
 	UNKOWN
 };
 
+enum Arity
+{
+	CONSTANT,
+	UNARY,
+	BINARY
+};
+
 struct Token
 {
 	std::string	value;
 	TokenType	type;
+	Arity		arity;
 };
 
 using VarType = std::variant<Real, Complex, Matrix>;
 
-struct MathVisitor
-{
-	std::string	op;
-
-	VarType	operator()(const Real& a, const Real& b) const
-	{
-		if (op == "+")
-			return (a + b);
-		if (op == "-")
-			return (a - b);
-		if (op == "*")
-			return (a * b);
-		if (op == "/")
-			return (a / b);
-		throw std::runtime_error("Unknown operator");
-	}
-	VarType operator()(const Matrix& a, const Matrix& b) const
-	{
-		if (op == "+")
-			return (a + b);
-		if (op == "-")
-			return (a - b);
-		if (op == "*")
-			return (a * b);
-		if (op == "/")
-			throw std::runtime_error("Matrix division not supported");
-		throw std::runtime_error("Unknown operator");
-	}
-	VarType operator()(const Real& scl, const Matrix& m) const
-	{
-		if (op == "*")
-			return (m * scl);
-		if (op == "+")
-			throw std::runtime_error("Cannot add scalar and matrix");
-		if (op == "-")
-			throw std::runtime_error("Cannot sub scalar and matrix");
-		if (op == "/")
-			throw std::runtime_error("Cannot divide scalar and matrix");
-		throw std::runtime_error("Unknown operator");
-	}
-	VarType operator()(const Complex& a, const Real& b) const
-	{
-		if (op == "+")
-			return (a + b);
-		if (op == "-")
-			return (a - b);
-		if (op == "*")
-			return (a * b);
-		if (op == "/")
-			return (a / b);
-		return (Complex(0.));
-	}
-	template<typename T, typename U>
-	VarType operator()(const T& a, const U& b)
-	{	
-		static_cast<void>(a);
-		static_cast<void>(b);
-		throw std::runtime_error("Invalid type operation");
-	}
-};
+VarType	apply_unary_op( const std::string& op, const VarType& a );
+VarType	apply_binary_op( const std::string& op, const VarType& lhs, const VarType& rhs );
 
 void	process_input( const std::vector<Token>&, std::map<std::string, VarType>& );
+void	pre_pass_arity( std::vector<Token>& tokens );
